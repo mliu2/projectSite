1d8db577a938bf550524ede2af417d4fb279363c
Mingzhao Liu <mz.liu0@gmail.com>
2019-03-11 19:47:33 -0400

    Test Cases added
    Test cases for user authentication for contact listing added

--- contactList/tests.py
+++ contactList/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class LoginTests(TestCase):
+    def test_contact_list_no_login(self):
+        url = reverse('contactList:contacts')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_contact_list_login(self):
+        #create and log user in
+        user = User.objects.create_user('test', 'test@test.com','somepassword')
+        self.client.login(username='test',password='somepassword')
+        url = reverse('contactList:contacts')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
